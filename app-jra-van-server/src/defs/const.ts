1dad219439ca82a9b3f706ec16641dba951eb676
yo16 <xxyo1xx@gmail.com>
2025-01-18 10:57:17 +0900

    using const for dir path

---
+++ app-jra-van-server/src/defs/const.ts
@@ -0,0 +1,15 @@
+// 固定値の定義
+
+// データフォルダ
+export const LOCALDATA_DIR_BASE = "./data";
+
+// テーブル定義(DDL)フォルダ
+export const DDL_DIR_BASE = "./ddl";
+
+// データタイプごとのフォルダ
+export const DATATYPE_DIR_BASE = {
+    "Accumulated": "/Accumulated",
+    "Realtime": "/Realtime",
+    "Setup": "/Setup",
+};
+
